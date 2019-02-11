67a01aa3253ea0edb10badce07d119f2ab730b24
Loïc HAYE <haye.loic@gmail.com>
2019-02-11 11:22:19 +0100

    add unit tests project and some tests

--- DotNetCoreExampleLibrary/AgeCalculator.cs
+++ DotNetCoreExampleLibrary/AgeCalculator.cs
@@ -2,9 +2,9 @@
 
 namespace DotNetCoreExampleLibrary
 {
-    class AgeCalculator
+    public class AgeCalculator
     {
-        int CalculateAge(DateTime birthDate, DateTime dateForAgeCalculation)
+        public int CalculateAge(DateTime birthDate, DateTime dateForAgeCalculation)
         {
             int age = dateForAgeCalculation.Year - birthDate.Year;
             if (birthDate > dateForAgeCalculation.AddYears(-age))
