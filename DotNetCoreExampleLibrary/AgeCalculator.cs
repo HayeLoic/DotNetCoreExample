1f3a37551a7e15b1765d302ebc109d72588f971d
Loïc HAYE <haye.loic@gmail.com>
2019-02-11 11:02:34 +0100

    library creation
    age calculator initialization

---
+++ DotNetCoreExampleLibrary/AgeCalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DotNetCoreExampleLibrary
+{
+    class AgeCalculator
+    {
+        int CalculateAge(DateTime birthDate, DateTime dateForAgeCalculation)
+        {
+            int age = dateForAgeCalculation.Year - birthDate.Year;
+            if (birthDate > dateForAgeCalculation.AddYears(-age))
+            {
+                age--;
+            }
+            return age;
+        }
+    }
+}
